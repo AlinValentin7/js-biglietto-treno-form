f32fd7b518983b74e9cab15a2c3b9275c6040275
AlinValentin7 <trandafiralionvalentin@gmail.com>
2024-07-03 13:39:40 +0200

    add: sconto

---
+++ app.js
@@ -0,0 +1,56 @@
+console.log("Treno ciuf ciuf")
+
+//recuperiamo il form
+const trainElement = document.getElementById("train")
+//dichiariamo le variabili che ci serevono e stampiamo 
+    const kmElement = document.getElementById("Km")
+    const ageElement = document.getElementById("age")
+    const ticketElement = document.getElementById("ticket")
+    const scontoElement = document.getElementById("sconto")
+
+    console.log(kmElement,ageElement,ticketElement,trainElement)
+
+//impediamo che il form venga inviato
+    trainElement.addEventListener('submit', function (event) {
+        event.preventDefault()
+   
+    // recuperiamo l 'eta inserita dal utente
+	const age = parseInt(ageElement.value) // number
+	console.log(age)
+
+     //recuperiamo i Km inseriti dal utente    
+     const km = parseFloat(kmElement.value) 
+     console.log(km)
+    //recuperiamo la select     
+     const sconto = scontoElement.value
+     let prezzo = 0.21
+     let prezzoBgilietto = km * prezzo
+
+     if(sconto === "Minorenne") {
+
+         prezzoBgilietto = (prezzoBgilietto * 20) / 100
+     }
+
+     
+     else if  ( sconto === "Senior") 
+     {
+        prezzoBgilietto = (prezzoBgilietto * 40) / 100
+
+     }
+
+     else(sconto === "")
+      {
+        prezzoBgilietto
+
+     }
+     
+
+
+
+
+
+     
+     console.log(prezzoBgilietto)
+     ticketElement.innerHTML = prezzoBgilietto + "£"
+
+    })
